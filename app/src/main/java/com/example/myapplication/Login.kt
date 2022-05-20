af3885482202e636b9a4db113b1a5e1db8be2d2c
rau13 <rauan5005@gmail.com>
2022-05-20 09:49:25 +0600

    AnalyticsApp

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.StrictMode
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.os.HandlerCompat.postDelayed
 import com.example.myapplication.responses.BTRXLeads
 import com.example.myapplication.responses.ResponseBTRXDeals
 import com.google.firebase.auth.FirebaseAuth
@@ -25,18 +28,31 @@ class Login : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var button:Button
-
+    private var totalDeals:String? = null
+    private var totalLeads:String? = null
     private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar!!.hide()
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        init()
+    }
+    private fun init(){
         auth = FirebaseAuth.getInstance()
         email = findViewById(R.id.editTextTextEmailAddress)
         password = findViewById(R.id.editTextTextPassword)
         button = findViewById(R.id.button)
         database = FirebaseDatabase.getInstance().getReference("Bitrix")
+        totalLeads()
+        totalDeals()
+        Handler().postDelayed(
+            {
+                setBTXDataDeals()
+                setBTXDataLeads()
+            },1200)
         button.setOnClickListener{
             login()
             button.setTextColor(resources.getColor(R.color.white))
@@ -55,8 +71,6 @@ class Login : AppCompatActivity() {
                 val intent = Intent(this, Overview::class.java)
                 intent.putExtra("email", email)
                 startActivity(intent)
-                setBTXDataDeals()
-                setBTXDataLeads()
                 button.setTextColor(resources.getColor(R.color.green_chart))
                 Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
             } else
@@ -68,7 +82,7 @@ class Login : AppCompatActivity() {
     }
 
     private fun setBTXDataLeads(){
-        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.lead.list.json").readText()
+        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.lead.list.json?start=$totalLeads").readText()
         var gson = Gson()
         var data = gson.fromJson(response, BTRXLeads::class.java)
         Log.d("Mylog","${data.total}")
@@ -96,10 +110,9 @@ class Login : AppCompatActivity() {
     }
     private fun setBTXDataDeals(){
         val database =  FirebaseDatabase.getInstance().getReference("BitrixDeals")
-        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.deal.list.json").readText()
+        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.deal.list.json?start=$totalDeals").readText()
         var gson = Gson()
         var data = gson.fromJson(response, ResponseBTRXDeals::class.java)
-        Log.d("Mylog","${data.total}")
         val delimiter1 = "T"
         val delimiter2 = "+"
 
@@ -123,6 +136,19 @@ class Login : AppCompatActivity() {
 
 
         }
+    }
+    private fun totalDeals(){
+        val database =  FirebaseDatabase.getInstance().getReference("BitrixDeals")
+        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.deal.list.json").readText()
+        var gson = Gson()
+        var data = gson.fromJson(response, ResponseBTRXDeals::class.java)
+        totalDeals = data.total.toString()
+    }
+    private fun totalLeads(){
+        var response = URL("https://estero.bitrix24.kz/rest/57/4sr1m84vu05m46bf/crm.lead.list.json").readText()
+        var gson = Gson()
+        var data = gson.fromJson(response, BTRXLeads::class.java)
+        totalLeads = data.total.toString()
     }
 }
 
